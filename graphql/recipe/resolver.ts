f0692ae5f33d5d2225c0fa9c996b571267f652a5
Timmy <shadow_tsw@web.de>
2021-04-06 21:28:42 +0200

    Update resolver.ts
    Template Recipe

--- graphql/recipe/resolver.ts
+++ graphql/recipe/resolver.ts
@@ -3,6 +3,38 @@ import Recipe from '../../models/recipe.schema';
 import { errName } from '../../utils/error/error-handler';
 import validator from 'validator';
 
-const GraphQLResolver = {};
+const GraphQLResolver = {
+  addRecipe: async function({},req:any){
+    if(!req.user){
+      throw new Error(errName.AUTH_FAILED);
+    }
+    
+    const {recipeName} = xxxx
+    
+    try{
+      
+      const newRecipe = new Recipe({
+        userID:req.user,
+        recipeName: recipeName,
+        ingredients: xxx,
+      })
+      
+      return await newRecipe.save();
+     
+    }catch (err){
+      throw err
+    }
+  },
+  updateRecipe:async function(){
+  },
+  like:async function(){
+  },
+  disLike:async function(){
+  },
+  addComment:async function(){
+  },
+  removeComment:async function(){
+  },
+};
 
 export default GraphQLResolver;
